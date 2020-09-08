7da234cde6e95de6904a69c4a1ceff33a2f11877
James Ayers <james@leapfrogdesign.co.nz>
2020-09-08 12:45:37 +1200

    Enhance: Combine Rating and Rating Block Admins

--- code/Admin/RatingAdmin.php
+++ code/Admin/RatingAdmin.php
@@ -3,18 +3,20 @@
 namespace DNADesign\Elemental\Admins;
 
 use SilverStripe\Admin\ModelAdmin;
+use DNADesign\Elemental\Models\Rating;
+use DNADesign\Elemental\Models\ElementRatingBlock;
 use Colymba\BulkManager\BulkManager;
 use Colymba\BulkManager\BulkAction\UnlinkHandler;
 use Colymba\BulkManager\BulkAction\EditHandler;
-use DNADesign\Elemental\Models\Rating;
 
 class RatingAdmin extends ModelAdmin
 {
     private static $managed_models = [
         Rating::class,
+        ElementRatingBlock::class,
     ];
 
-    private static $menu_title = 'User ratings';
+    private static $menu_title = 'Ratings';
 
     private static $url_segment = 'ratings';
 
@@ -24,14 +26,24 @@ class RatingAdmin extends ModelAdmin
     {
         $form = parent::getEditForm($id, $fields);
 
-        $field = $form->Fields()->dataFieldByName($this->sanitiseClassName(Rating::class));
+        $field = $form->Fields()->dataFieldByName($this->sanitiseClassName($this->modelClass));
         $config = $field->getConfig();
 
-        // Add bulk option
-        $manager = new BulkManager();
-        $manager->removeBulkAction(EditHandler::class);
-        $manager->removeBulkAction(UnlinkHandler::class);
-        $config->addComponent($manager);
+        if ($this->modelClass === Rating::class) {
+            // Add bulk option
+            $manager = new BulkManager();
+            $manager->removeBulkAction(EditHandler::class);
+            $manager->removeBulkAction(UnlinkHandler::class);
+            $config->addComponent($manager);
+        }
+
+        if ($this->modelClass === ElementRatingBlock::class) {
+            $config->removeComponentsByType(GridFieldArchiveAction::class);
+            $config->removeComponentsByType(GridFieldDeleteAction::class);
+            $config->removeComponentsByType(GridFieldExportButton::class);
+            $config->removeComponentsByType(GridFieldPrintButton::class);
+            $config->removeComponentsByType(GridFieldImportButton::class);
+        }
 
         return $form;
     }
